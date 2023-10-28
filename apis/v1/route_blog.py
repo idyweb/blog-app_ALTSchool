d8a58491ba6fe28b12fe4c1f12cfe12f4c7dfb37
Idongesit Inyang <inyangidongesit22@gmail.com>
2023-10-28 16:14:49 +0100

    user authorization

--- apis/v1/route_blog.py
+++ apis/v1/route_blog.py
@@ -6,6 +6,8 @@ from fastapi import HTTPException
 from fastapi import status
 from sqlalchemy.orm import Session
 
+from apis.v1.route_login import get_current_user
+from db.models.user import User
 from db.repository.blog import create_new_blog
 from db.repository.blog import delete_blog
 from db.repository.blog import list_blogs
@@ -43,16 +45,28 @@ def get_blog(id: int, db: Session = Depends(get_db)):
 
 
 @router.put("/blog/{id}", response_model=ShowBlog)
-def update_a_blog(id: int, blog: UpdateBlog, db: Session = Depends(get_db)):
-    blog = update_blog(id=id, blog=blog, author_id=1, db=db)
-    if not blog:
-        raise HTTPException(detail=f"Blog with id {id} does not exist")
+def update_a_blog(
+    id: int,
+    blog: UpdateBlog,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+):
+    blog = update_blog(id=id, blog=blog, author_id=current_user.id, db=db)
+    if isinstance(blog, dict):
+        raise HTTPException(
+            detail=blog.get("error"),
+            status_code=status.HTTP_404_NOT_FOUND,
+        )
     return blog
 
 
 @router.delete("/delete/{id}")
-def delete_a_blog(id: int, db: Session = Depends(get_db)):
-    message = delete_blog(id=id, author_id=1, db=db)
+def delete_a_blog(
+    id: int,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+):
+    message = delete_blog(id=id, author_id=current_user.id, db=db)
     if message.get("error"):
         raise HTTPException(
             detail=message.get("error"), status_code=status.HTTP_400_BAD_REQUEST
--- schemas/blog.py
+++ schemas/blog.py
@@ -7,11 +7,13 @@ from pydantic import BaseModel
 class CreateBlog(BaseModel):
     title: str
     content: Optional[str] = None
+    author_id: int
 
 
 class ShowBlog(BaseModel):
     title: str
     content: Optional[str]
+    author_id: int
     created_at: datetime
 
     class Config:
@@ -19,4 +21,7 @@ class ShowBlog(BaseModel):
 
 
 class UpdateBlog(CreateBlog):
-    pass
+    title: str
+    content: str
+    author_id: int
+    created_at: datetime
